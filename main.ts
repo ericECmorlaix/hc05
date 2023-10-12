f29d0bd38912b11342d4b4db0f45cd2d4a82645e
Eric MADEC <eric.madec@ecmorlaix.fr>
2023-10-12 11:53:42 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,14 @@
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     OLED.writeStringNewLine(serial.readLine())
 })
-input.onButtonPressed(Button.A, function () {
-    serial.writeLine("AT")
-    OLED.writeStringNewLine(serial.readString())
-    basic.pause(1000)
-    serial.writeLine("AT+NAME=myhc05")
-    OLED.writeStringNewLine(serial.readString())
-    basic.pause(1000)
-    serial.writeLine("AT+PSWD=1234")
-    basic.pause(1000)
-    OLED.writeStringNewLine(serial.readString())
-    basic.pause(1000)
-    serial.writeLine("AT")
-    OLED.writeStringNewLine(serial.readString())
-    basic.pause(1000)
-})
 input.onButtonPressed(Button.B, function () {
     serial.writeLine("Hello World !")
 })
-OLED.init(64, 158)
+OLED.init(64, 128)
 serial.redirect(
 SerialPin.P8,
 SerialPin.P12,
-BaudRate.BaudRate38400
+BaudRate.BaudRate9600
 )
 basic.forever(function () {
 	
